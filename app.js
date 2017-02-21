68a6ac7468af4d6d042d09e3fc1668768d984b87
Wojtek Janoszek <wetcouch@icloud.com>
2017-02-21 19:11:48 +0100

    feat: Checkboxes define both string and number value

--- app.js
+++ app.js
@@ -1,84 +1,51 @@
 /**
  * Created by wetcouch on 07.02.2017.
  */
-class Permission {
-    toStr(num) {
-        let str = num.toString().split("");
-        // 4 = Read, 2 = Write, 1 = eXecute
-        for (let i = 0; i < str.length; i++) {
-            if (str[i] == 0) {
-                str[i] = '---'
-            } else if (str[i] == 1) {
-                str[i] = '--x'
-            } else if (str[i] == 2) {
-                str[i] = '-w-'
-            } else if (str[i] == 3) {
-                str[i] = '-wx'
-            } else if (str[i] == 4) {
-                str[i] = 'r--'
-            } else if (str[i] == 5) {
-                str[i] = 'r-x'
-            } else if (str[i] == 6) {
-                str[i] = 'rw-'
-            } else if (str[i] == 7) {
-                str[i] = 'rwx'
-            }
-        }
-        str = str.join("");
-        return str;
-    }
-
-    toNum(str) {
-        let num = str.split("");
-        let count =  0;
-        let part = 0;
-        let newArr = [];
-        if(num.length > 9) {
-            num.shift();
-        }
-
-        for (let i = 0; i < num.length; i++) {
 
 
-            if (num[i] == 'r') {
-                part += 4;
-                count++
-            } else if (num[i] == 'w') {
-                part += 2;
-                count++
-            } else if (num[i] == 'x') {
-                part+= 1;
-                count++
-            } else if (num[i] == '-') {
-                count++
-            }
-
-            if (count >= 3) {
-                count = 0;
-                newArr.push(part);
-                part = 0;
-            }
-        }
-        return newArr;
-    }
-}
-
-let perm = new Permission();
-let numVal = document.getElementById('numVal');
-let strVal = document.getElementById('strVal');
-
-function convertNum () {
-    strVal.value = perm.toStr(numVal.value);
-}
-
-function convertStr () {
-    numVal.value = perm.toNum(strVal.value);
-}
 
 new Vue({
     el: '#app',
     data: {
-        permss: []
+        permss: [],
+        mesStr: [],
+        mesNum: []
+    },
+    methods: {
+        calculate: function () {
+            this.mesStr = [];
+            this.mesNum = [];
+            let num = [];
+            num[1] = 0;
+            num[2] = 0;
+            num[3] = 0;
+            
+            for (i = 1; i <= 3; i++) {
+                if (this.permss.includes('read'+i)) {
+                    this.mesStr.push('r');
+                    num[i] += 4;
+                } else {
+                    this.mesStr.push('-')
+                }
+
+                if (this.permss.includes('write'+i)) {
+                    this.mesStr.push('w');
+                    num[i] += 2;
+                } else {
+                    this.mesStr.push('-')
+                }
+
+                if (this.permss.includes('execute'+i)) {
+                    this.mesStr.push('x');
+                    num[i] += 1;
+                } else {
+                    this.mesStr.push('-')
+                }
+            }
+            this.mesNum.push(num[1], num[2], num[3]);
+            this.mesNum = this.mesNum.join("");
+            this.mesStr = this.mesStr.join("");
+        }
     }
 
 
