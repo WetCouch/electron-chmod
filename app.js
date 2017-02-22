57031db6f56ec5ce2b2ca09424c0181780b7b4de
Wojtek Janoszek <wetcouch@icloud.com>
2017-02-22 21:30:47 +0100

    feat: convert number to string and string to number by buttons

--- app.js
+++ app.js
@@ -4,22 +4,22 @@
 
 
 
-new Vue({
+let app = new Vue({
     el: '#app',
     data: {
         permss: [],
-        mesStr: [],
-        mesNum: []
+        mesStr: '---------',
+        mesNum: '000'
     },
-    methods: {
-        calculate: function () {
+    watch: {
+        permss: function () {
             this.mesStr = [];
             this.mesNum = [];
             let num = [];
             num[1] = 0;
             num[2] = 0;
             num[3] = 0;
-            
+
             for (i = 1; i <= 3; i++) {
                 if (this.permss.includes('read'+i)) {
                     this.mesStr.push('r');
@@ -45,6 +45,68 @@ new Vue({
             this.mesNum.push(num[1], num[2], num[3]);
             this.mesNum = this.mesNum.join("");
             this.mesStr = this.mesStr.join("");
+
+        }
+    },
+    methods: {
+        calcMesNum: function () {
+            this.permss = [];
+            let num = this.mesNum.split("");
+            for (i = 1; i <= 3; i++)
+            if (num[i-1] == 7) {
+                this.permss.push('read' + i);
+                this.permss.push('write' + i);
+                this.permss.push('execute' + i);
+            } else if (num[i-1] == 6) {
+                this.permss.push('read' + i);
+                this.permss.push('write' + i);
+            } else if (num[i-1] == 5) {
+                this.permss.push('read' + i);
+                this.permss.push('execute' + i);
+            } else if (num[i-1] == 4) {
+                this.permss.push('read' + i);
+            } else if (num[i-1] == 3) {
+                this.permss.push('write' + i);
+                this.permss.push('execute' + i);
+            } else if (num[i-1] == 2) {
+                this.permss.push('write' + i);
+            } else if (num[i-1] == 1) {
+                this.permss.push('execute' + i);
+            }
+        },
+        calcMesStr: function () {
+            this.permss = [];
+            let num = this.mesStr.split("");
+            let count = 0;
+            let part = 0;
+            let newArr = [];
+            if(num.length > 9) {
+                num.shift();
+            }
+
+            for (let i = 0; i < num.length; i++) {
+
+                if (num[i] == 'r') {
+                    part += 4;
+                    count++
+                } else if (num[i] == 'w') {
+                    part += 2;
+                    count++
+                } else if (num[i] == 'x') {
+                    part+= 1;
+                    count++
+                } else if (num[i] == '-') {
+                    count++
+                }
+
+                if (count >= 3) {
+                    count = 0;
+                    newArr.push(part);
+                    part = 0;
+                }
+            }
+            this.mesNum = newArr.join("");
+            this.calcMesNum();
         }
     }
 
